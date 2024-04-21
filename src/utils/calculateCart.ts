17119931be5d3e30070932c4b3941f64f892521b
Denis Zhmurenko <hi@dyzoon.dev>
2024-04-21 16:03:46 +0100

    Small fixes (#40)

--- src/utils/calculateCart.ts
+++ src/utils/calculateCart.ts
@@ -4,5 +4,5 @@ import { Promocode } from "../types/domain";
 export const calculateCart = (cart: CartStoreType['cart'], promocode?: Promocode | null) => ({
     totalQuantity: cart.reduce((sum, { quantity }) => sum + quantity, 0),
     subTotalSum: cart.reduce((sum, { item, quantity }) => sum + item.price * quantity, 0),
-    totalSum: cart.reduce((sum, { item, quantity }) => sum + item.price * quantity, 0) * ((100 - (promocode?.discount || 0)) / 100)
+    totalSum: Math.round((cart.reduce((sum, { item, quantity }) => sum + item.price * quantity, 0) * ((100 - (promocode?.discount || 0)) / 100) + Number.EPSILON) * 100) / 100
 });
